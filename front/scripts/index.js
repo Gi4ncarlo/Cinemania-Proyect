cd7ecdb8148dda86107778fc25991a291c828073
Giancarlo <giancarlozanarini9@gmail.com>
2024-05-23 19:12:31 -0300

    Primer entrega Proyecto Full Stack M2

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,1 +1,53 @@
 console.log(tempData);
+
+function showMovies(tempData) {
+  const articlePelicula = document.createElement("article");
+  articlePelicula.classList.add("tarjeta");
+
+  const imagen = document.createElement("img");
+  imagen.classList.add("tarjeta-imagen");
+
+  const infoContainer = document.createElement("div");
+  infoContainer.classList.add("tarjeta-info");
+
+  const titleHeading = document.createElement("h2");
+  const anio = document.createElement("p");
+  const dire = document.createElement("p");
+  const duracion = document.createElement("p");
+  const genero = document.createElement("p");
+  const puntuacion = document.createElement("p");
+
+  const contenedor = document.querySelector(".peliculas");
+
+  const { title, year, director, duration, genre, rate, poster } = tempData;
+
+  titleHeading.innerHTML = title;
+  anio.innerHTML = `<strong>Año :</strong> ${year}`;
+  dire.innerHTML = `<strong>Director :</strong> ${director}`;
+  duracion.innerHTML = `<strong>Duración :</strong> ${duration}`;
+  genero.innerHTML = `<strong>Género :</strong> ${genre}`;
+  puntuacion.innerHTML = `<strong>Puntuación :</strong> ${rate}`;
+  imagen.src = poster;
+  imagen.alt = `Poster de ${title}`;
+
+  infoContainer.appendChild(titleHeading);
+  infoContainer.appendChild(anio);
+  infoContainer.appendChild(dire);
+  infoContainer.appendChild(duracion);
+  infoContainer.appendChild(genero);
+  infoContainer.appendChild(puntuacion);
+
+  articlePelicula.appendChild(imagen);
+  articlePelicula.appendChild(infoContainer);
+
+  titleHeading.classList.add("tarjeta-titulo");
+  genero.classList.add("tarjeta-genero");
+
+  contenedor.appendChild(articlePelicula);
+}
+
+function init(){
+  tempData.map((peli) => showMovies(peli));
+};
+
+init()
--- front/scripts/tempData.js
+++ front/scripts/tempData.js
@@ -29,4 +29,42 @@ const tempData = [
     poster:
       "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg",
   },
+  {
+    title: "Godzilla y Kong: El nuevo imperio",
+    year: 2024,
+    director: "Adam Wingard",
+    duration: "1h 55min",
+    genre: ["Action","Fantasy"],
+    rate: 9.5,
+    poster:"https://img.asmedia.epimg.net/resizer/v2/R2QZNYBIYBETBHAJGBBVFNS5WM.jpg?auth=d889ebd70a610dc819c809e72ce739142564fee544f0a1621e99ab365b9229e4&width=1472&height=1104&smart=true"
+  },
+  {
+    title: "Megamente contra el Sindicato de la Perdición",
+    year: 2024,
+    director: "Eric Fogel",
+    duration: "1h 25min",
+    genre: ["Comedia","Fantasy", "Cine SuperHeroe"],
+    rate: 7.5,
+    poster:"https://br.web.img3.acsta.net/img/53/93/5393cbc0b035346e06429049c8351db5.jpg",
+  },
+  {
+    title: "El reino del planeta de los simios",
+    year: 2024,
+    director: "Wes Ball",
+    duration: "2h 25min",
+    genre: ["Ciencia ficción", "Aventuras", "Simios", "Futuro ",],
+    rate: 9.9,
+    poster:"https://img.rtve.es/imagenes/cartel-reino-del-planeta-simios/01715175743713.jpg",
+  },
+  {
+    title: "Top Gun Maverick",
+    year: 2022,
+    director: "Joseph Kosinski",
+    duration: "2h 11min",
+    genre: ["Acción","Ejército", "Aviones", "Secuela"],
+    rate: 9.7,
+    poster:"https://musicart.xboxlive.com/7/8fa06100-0000-0000-0000-000000000002/504/image.jpg?w=1920&h=1080",
+  },
+
 ];
+
